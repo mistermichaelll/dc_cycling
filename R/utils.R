15745ad7390ed214f6c350a832a87c8e053e40aa
Michael Johnson <mistermichaellll@gmail.com>
2023-02-05 16:12:31 -0500

    utility scripts

---
+++ R/utils.R
@@ -0,0 +1,33 @@
+connect_to_db <- function() {
+  host <- "localhost"
+  user <- Sys.getenv("DB_USERNAME")
+  pass <- Sys.getenv("DB_PASSWORD")
+  
+  conn <- DBI::dbConnect(
+    RPostgres::Postgres(), 
+    "postgres",
+    host = host, 
+    user = user, 
+    password = pass
+  )
+  
+  conn
+}
+
+query_db <- function(conn, statement) {
+  result <- DBI::dbGetQuery(
+    conn, 
+    statement
+  )
+  
+  tibble::tibble(result)
+}
+
+write_to_db <- function(conn, table_name, table, ...) {
+  DBI::dbWriteTable(
+    conn,
+    DBI::Id(schema = "dc_open_data_raw", table = table_name), 
+    table, 
+    ...
+  )
+}
